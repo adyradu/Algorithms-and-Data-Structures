5eb71cca1841fb4551c59c0891f600f718263e86
Adrian-Leonard Radu <ady8radu@gmail.com>
2020-02-27 00:15:35 -0800

    Fixed case when it points to self.

--- lists/copy_list_with_random_ptr/copy_list_with_random_ptr.cpp
+++ lists/copy_list_with_random_ptr/copy_list_with_random_ptr.cpp
@@ -1,3 +1,7 @@
+// Given a list with next pointer and a random pointer
+// return a deep copy of the list.
+// O(n) time, O(n) space
+//
 #include<iostream>
 #include<unordered_map>
 
@@ -11,56 +15,60 @@ public:
     Node* next;
     Node* random;
     
-    Node(int _val) {
+    Node (int _val) {
         val = _val;
-        next = NULL;
-        random = NULL;
+        next = nullptr;
+        random = nullptr;
     }
 };
 
 Node* copyRandomList(Node* head) {
-        Node* cur = head;
-        Node* newHead = nullptr;
-        Node* newCur = nullptr;
-        Node* newPrev = nullptr;
-        unordered_map<Node*, Node*> nodeMap;
-        
-        while (cur) {
-            newCur = new Node(cur->val);
-            
-            nodeMap.insert(pair<Node*, Node*>(cur, newCur));
-            
-            if (newPrev) {
-                newPrev->next = newCur;
-            }
-            else {
-                newHead = newCur;
-            }
-            
-            newPrev = newCur;
-            cur = cur->next;
-        }
-        
-        newCur = newHead;
-        cur = head;
-        
-        while (newCur && cur) {
-            if (cur->random == nullptr) {
-                newCur->random = nullptr;
-            }
-            else if (cur->random == cur) {
-                newCur->random = cur;
-            }
-            else {
-                newCur->random = nodeMap[cur->random];
-            }
-            
-            newCur = newCur->next;
-            cur = cur->next;
-        }
-        
-        return newHead;
+    Node* cur = head;
+    Node* newHead = nullptr;
+    Node* newCur = nullptr;
+    Node* newPrev = nullptr;
+    unordered_map<Node*, Node*> nodeMap;
+    
+    if (!head) {
+        return nullptr;
     }
+    
+    newHead = new Node(head->val);
+    nodeMap.insert(pair<Node*, Node*>(head, newHead));
+    cur = cur->next;
+    newCur = newHead;
+    newPrev = newCur;
+    
+    while (cur) {
+        newCur = new Node(cur->val);
+        
+        nodeMap.insert(pair<Node*, Node*>(cur, newCur));
+        
+        newPrev->next = newCur;
+        newPrev = newCur;
+        cur = cur->next;
+    }
+    
+    newCur = newHead;
+    cur = head;
+    
+    while (newCur) {
+        if (cur->random == nullptr) {
+            newCur->random = nullptr;
+        }
+        else if (cur->random == cur) {
+            newCur->random = newCur;
+        }
+        else {
+            newCur->random = nodeMap[cur->random];
+        }
+        
+        newCur = newCur->next;
+        cur = cur->next;
+    }
+    
+    return newHead;
+}
 
 int main(void)
 {
